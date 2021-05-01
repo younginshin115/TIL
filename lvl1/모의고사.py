bd73776d8978843236d33bd29f1161aa17d79954
Youngin Shin <82853811+younginshin115@users.noreply.github.com>
2021-05-01 17:47:09 +0900

    Create 모의고사.py

---
+++ lvl1/모의고사.py
@@ -0,0 +1,29 @@
+def solution(answers):
+    answer = []
+    def student(answers, pattern):
+        num = 0
+        correct_num = 0
+        for i in range(len(answers)):
+            if i != 0 and i % len(pattern) == 0:
+                num += len(pattern)
+            if answers[i] == pattern[i-num]:
+                correct_num += 1
+        return correct_num
+    def grade(*scores):
+        total_lst = []
+        for i in scores:
+            total_lst.append(i)
+        max_score = max(total_lst)
+        for i in range(len(scores)):
+            if scores[i] == max_score:
+                answer.append(i+1)
+        
+    st1_pattern = [1,2,3,4,5]
+    st2_pattern = [2, 1, 2, 3, 2, 4, 2, 5]
+    st3_pattern = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    s1 = student(answers, st1_pattern)    
+    s2 = student(answers, st2_pattern)
+    s3 = student(answers, st3_pattern)
+    grade(s1, s2, s3)
+
+    return answer
